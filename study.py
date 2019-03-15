19ebb89d751b880dfb3d8ab975cfe5278601c9ac
hubhack <2329259856@qq.com>
2019-03-15 19:52:33 +0800

    add

--- study.py
+++ study.py
@@ -1,3 +1,4 @@
+'''
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget
 if __name__ == "__main__":
@@ -10,4 +11,28 @@ if __name__ == "__main__":
     w.move(300,300)
     w.setWindowTitle ('简单')
     w.show()
+    sys.exit(app.exec_())'''
+import sys
+from PyQt5.QtWidgets import  QMainWindow,QApplication
+
+class Exampel(QMainWindow):
+
+    def __init__(self):
+        supper().__init__()
+
+
+
+        self.initUI()
+
+    def intiUI(self):
+        self.statusBar().showMessage('Ready')
+
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+
+if __name__ == "__main__":
+    
+    app = QApplication(sys.argv)   
+    ex = Example()
     sys.exit(app.exec_())
