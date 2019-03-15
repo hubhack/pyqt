8737ace1f3ad76236f66cf18f60b12c2faafbb72
hubhack <2329259856@qq.com>
2019-03-15 19:27:59 +0800

    add

--- study.py
+++ study.py
@@ -1,10 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget
 if __name__ == "__main__":
+
     app  = QApplication(sys.argv)
 
 
     w = QWidget()
     w.resize(250,150)
     w.move(300,300)
-    w,setWidowTitle('简单')
+    w.setWindowTitle ('简单')
     w.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
